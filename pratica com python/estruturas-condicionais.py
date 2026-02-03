46190b5bf936c8bef44f24c031ae9287c7007c64
Viniciusxz <viniciusribeirolinsr@gmail.com>
2026-02-03 14:42:26 -0300

    Sistema de alunos aprovados ou reprovados.

---
+++ pratica com python/estruturas-condicionais.py
@@ -0,0 +1,20 @@
+# Estruturas condicionais com IF, ELSE e ELIF
+
+idade_maria = int(input('Digite a idade da Maria: '))
+idade_beatriz = int(input('Digite a idade da Beatriz: '))
+
+if idade_maria > idade_beatriz:
+  print('Maria é mais velha que Beatriz.')
+
+
+# Sistema de alunos aprovados ou reprovados.
+
+media = float(input('Digite a média: '))
+
+if media >= 6.0:
+  print('Aprovado(a)!')
+else:
+  print('Reprovado(a)!')
+
+# Sistema com recuperação
+
