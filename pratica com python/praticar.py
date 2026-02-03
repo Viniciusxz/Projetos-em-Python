00b14486f856c388af36a3aff3774fcc8a794ac2
Viniciusxz <viniciusribeirolinsr@gmail.com>
2026-02-03 14:16:57 -0300

    Exemplos práticos com python

---
+++ pratica com python/praticar.py
@@ -0,0 +1,36 @@
+print("Python funcionando!")
+print("Escola de Dados da Alura!")
+
+print("Nome: [Vinícius]")
+print("Sobrenome: [Ribeiro]")
+
+print("V")
+print("I")
+print("N")
+print("I")
+print("C")
+print("I")
+print("U")
+print("S")
+
+data = 12
+mes = 'dezembro'
+ano = 2025
+
+print(f'{data} {mes} {ano}')
+
+ano2 = 2025
+print(f'Ano atual: {ano2}')
+
+nome = str(input('Olá!, insira seu nome aqui -> '))
+idade = int(input('Não esqueça da sua idade! :) -> '))
+altura = int(input('E também sua altura em metros! -> '))
+print(f'Olá, {nome}, Você tem {idade} de idade e com {altura} metros de altura! Uau! :)')
+
+# Soma de 2 valores numericos 
+
+soma1 = int(input('Digite um valor aqui! -> '))
+soma2 = int(input('Digite outro valor aqui! -> '))
+soma_total = soma1 + soma2
+print(soma_total)
+
