08ee2052e19930bfe6145ce2ec6b3781a83f188d
Viniciusxz <viniciusribeirolinsr@gmail.com>
2026-02-04 21:23:49 -0300

    Sistema para analisar valores que formam um triangulo, e dizer se são equilatéro, isósceles ou escaleo.

--- pratica com python/Exercicios-Python.py
+++ pratica com python/Exercicios-Python.py
@@ -145,21 +145,35 @@ print(f'Seu número é {par_impar}.')
 print(f'Seu número é {positivo_negativo}.')
 print(f'Seu número é {numero_decimal}.')
 
-
-#  se é par ou ímpar, positivo ou negativo e inteiro ou decimal.
-
-
-
 # 11) Escreva um programa que peça à pessoa usuária três números que representam os lados de um triângulo. O programa deve informar se os valores podem ser utilizados para formar um triângulo e, caso afirmativo, se ele é equilátero, isósceles ou escaleno. Tenha em mente algumas dicas:
 
 # Três lados formam um triângulo quando a soma de quaisquer dois lados for maior que o terceiro;
 # Triângulo Equilátero: três lados iguais;
 # Triângulo Isósceles: quaisquer dois lados iguais;
 # Triângulo Escaleno: três lados diferentes.
+
+triangulo_valor1 = float(input('Digite um valor númerico aqui: '))
+triangulo_valor2 = float(input('Digite um valor númerico aqui: '))
+triangulo_valor3 = float(input('Digite um valor númerico aqui: '))
+
+if triangulo_valor1 + triangulo_valor2 > triangulo_valor3 and triangulo_valor1 + triangulo_valor3 > triangulo_valor2 and triangulo_valor2 + triangulo_valor3 > triangulo_valor1:
+
+    print('Triangulo formado!')
+
+    if triangulo_valor1 == triangulo_valor2 == triangulo_valor3:
+        print('Você obteve um Triângulo Equilátero!')
+    elif triangulo_valor1 == triangulo_valor2 or triangulo_valor1 == triangulo_valor3 or triangulo_valor2 == triangulo_valor3:
+        print('Você obteve um Triângulo Isósceles')
+    else:
+        print('Você obteve um Triângulo Escaleno')
+
+else:
+    print('Os valores inseridos não formam um triângulo.')    
 # 12) Um estabelecimento está vendendo combustíveis com descontos variados. Para o etanol, se a quantidade comprada for até 15 litros, o desconto será de 2% por litro. Caso contrário, será de 4% por litro. Para o diesel, se a quantidade comprada for até 15 litros, o desconto será de 3% por litro. Caso contrário, será de 5% por litro. O preço do litro de diesel é R$ 2,00 e o preço do litro de etanol é R$ 1,70. Escreva um programa que leia a quantidade de litros vendidos e o tipo de combustível (E para etanol e D para diesel) e calcule o valor a ser pago pelo cliente. Tenha em mente algumas dicas:
 
 # O do valor do desconto será a multiplicação entre preço do litro, quantidade de litros e o valor do desconto.
 # O valor a ser pago por um cliente será o resultado da multiplicação do preço do litro pela quantidade de litros menos o valor de desconto resultante do cálculo.
+
 # 13) Em uma empresa de venda de imóveis você precisa criar um código que analise os dados de vendas anuais para ajudar a diretoria na tomada de decisão. O código precisa coletar os dados de quantidade de venda durante os anos de 2022 e 2023 e fazer um cálculo de variação percentual. A partir do valor da variação, deve ser enviada às seguintes sugestões:
 
 # Para variação acima de 20%: bonificação para o time de vendas.
