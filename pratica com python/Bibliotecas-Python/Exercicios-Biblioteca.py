bb174ed59cc9c5544d05428d9d4aa40665376695
Viniciusxz <viniciusribeirolinsr@gmail.com>
2026-02-12 15:15:14 -0300

    Sorteios de pessoas e token de acesso aleatório com randrange

--- pratica com python/Bibliotecas-Python/Exercicios-Biblioteca.py
+++ pratica com python/Bibliotecas-Python/Exercicios-Biblioteca.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib as mp
-from random import choice, randrange
+from random import choice, randrange, randint 
 # 1. Escreva um código para instalar a versão 3.7.1 da biblioteca matplotlib.
 
 # crtl+shift+' e digitar no terminal python -m pip install matplotlib'
@@ -28,20 +28,30 @@ print(f'Sua sorte hoje será de {listaVazia}.')
 num1 = int(input('Digite aqui um número!: '))
 num2 = int(input('Digite aqui um outro número!: '))
 
-resultado = pow(num1, num2)
+resultado = pow(num1, num2) # pow serve para calcular a potencia de 2 numeros selecionados, sendo num1 a base e num2 a potencia.
 print(f'Basicamente, o primeiro número que voce digitou é a base, e o segundo é o expoente! Vamos ver quanto é e... {resultado}.')
-
-# Dica: use a função pow() da biblioteca math
 
 # Aplicando a projetos
 # 6. Um programa deve ser escrito para sortear uma pessoa seguidora de uma rede social para ganhar um prêmio. A lista de participantes é numerada e devemos escolher aleatoriamente um número de acordo com a quantidade de participantes. Peça à pessoa usuária para fornecer o número de participantes do sorteio e devolva para ela o número sorteado.
+print('SORTEIO ONLINE')
+sorteio1 = int(input('Digite aqui quantas pessoas irão participar, e realizaremos um sorteio!: '))
 
-# 7. Você recebeu uma demanda para gerar números de token para acessar o aplicativo de uma empresa. O token precisa ser par e variar de 1000 até 9998. Escreva um código que solicita à pessoa usuária o seu nome e exibe uma mensagem junto a esse token gerado aleatoriamente.
+sorteio2 = randrange(1, (sorteio1 + 1))
+print(f'A pessoa sorteada foi: {sorteio2}. Parabéns!')
+
+# 7. Você recebeu uma demanda para gerar números de token para acessar o aplicativo de uma empresa. 
+# O token precisa ser par e variar de 1000 até 9998. Escreva um código que solicita à pessoa usuária o seu nome e exibe uma mensagem junto a esse token gerado aleatoriamente.
+
+nome = str(input('Digite seu nome aqui: '))
+token = randrange(1000, 9998, 2)
+
+print(f'Olá, {nome}, o seu token de acesso é {token}! Seja bem vindo(a)!')
+    
+
+
 
 # "Olá, [nome], o seu token de acesso é [token]! Seja bem-vindo(a)!"
 
-# 8. Para diversificar e atrair novos(as) clientes, uma lanchonete criou um item misterioso em seu cardápio chamado "salada de frutas surpresa". Neste item, são escolhidas aleatoriamente 3 frutas de uma lista de 12 para compor a salada de frutas da pessoa cliente. Crie o código que faça essa seleção aleatória de acordo com a lista abaixo:
-
-
-
+# 8. Para diversificar e atrair novos(as) clientes, uma lanchonete criou um item misterioso em seu cardápio chamado "salada de frutas surpresa". 
+# Neste item, são escolhidas aleatoriamente 3 frutas de uma lista de 12 para compor a salada de frutas da pessoa cliente. Crie o código que faça essa seleção aleatória de acordo com a lista abaixo:
 
