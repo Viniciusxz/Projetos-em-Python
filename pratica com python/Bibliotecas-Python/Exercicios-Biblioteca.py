1149c12e7d32bf96839b10cf8467bbf9b836ddc3
Viniciusxz <viniciusribeirolinsr@gmail.com>
2026-02-12 14:50:54 -0300

    Praticando com mathplotlib

---
+++ pratica com python/Bibliotecas-Python/Exercicios-Biblioteca.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib as mp
+from random import choice, randrange
+# 1. Escreva um código para instalar a versão 3.7.1 da biblioteca matplotlib.
+
+# crtl+shift+' e digitar no terminal python -m pip install matplotlib'
+
+# 2. Escreva um código para importar a biblioteca numpy com o alias np.
+
+# import numpy as np
+
+# 3. Crie um programa que leia a seguinte lista de números e escolha um número desta aleatoriamente.
+# lista = [8, 12, 54, 23, 43, 1, 90, 87, 105, 77]
+
+lista = [8, 12, 54, 23, 43, 1, 90, 87, 105, 77]
+sorteio = choice(lista) # choice serve para sortear um número aleatorio.
+print(f'O número sorteado para ser escolhido foi {sorteio}.')
+
+# 4. Crie um programa que sorteia, aleatoriamente, um número inteiro positivo menor que 100.
+print('Vamos ver quanta sorte você terá hoje, da escla de 0 até 100!')
+listaVazia = []
+for i in range(0,1):
+    listaVazia.append(randrange(100))
+
+print(f'Sua sorte hoje será de {listaVazia}.')
+# 5. Crie um programa que solicite à pessoa usuária digitar dois números inteiros e calcular a potência do 1º número elevado ao 2º.
+
+num1 = int(input('Digite aqui um número!: '))
+num2 = int(input('Digite aqui um outro número!: '))
+
+resultado = pow(num1, num2)
+print(f'Basicamente, o primeiro número que voce digitou é a base, e o segundo é o expoente! Vamos ver quanto é e... {resultado}.')
+
+# Dica: use a função pow() da biblioteca math
+
+# Aplicando a projetos
+# 6. Um programa deve ser escrito para sortear uma pessoa seguidora de uma rede social para ganhar um prêmio. A lista de participantes é numerada e devemos escolher aleatoriamente um número de acordo com a quantidade de participantes. Peça à pessoa usuária para fornecer o número de participantes do sorteio e devolva para ela o número sorteado.
+
+# 7. Você recebeu uma demanda para gerar números de token para acessar o aplicativo de uma empresa. O token precisa ser par e variar de 1000 até 9998. Escreva um código que solicita à pessoa usuária o seu nome e exibe uma mensagem junto a esse token gerado aleatoriamente.
+
+# "Olá, [nome], o seu token de acesso é [token]! Seja bem-vindo(a)!"
+
+# 8. Para diversificar e atrair novos(as) clientes, uma lanchonete criou um item misterioso em seu cardápio chamado "salada de frutas surpresa". Neste item, são escolhidas aleatoriamente 3 frutas de uma lista de 12 para compor a salada de frutas da pessoa cliente. Crie o código que faça essa seleção aleatória de acordo com a lista abaixo:
+
+
+
+
--- pratica com python/Bibliotecas-Python/grafico.py
+++ pratica com python/Bibliotecas-Python/grafico.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
+from random import choice
 
 estudantes = ['João', 'Maria', 'José']
+estudantes_2 = ['João', 'Maria', 'José']
 notas = [8.5, 9, 6.5]
 
 plt.bar(x = estudantes, height = notas)
-plt.show()
+estudante = choice(estudantes_2)
+print(f'o Estudante sorteado foi {estudante}') 
+plt.show() 
