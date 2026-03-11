31d1dbce5d2091cf5782d93ef60fb911579bfd93
Viniciusxz <viniciusribeirolinsr@gmail.com>
2026-03-11 15:18:05 -0300

    Sistema para armazenar elementos especificos de uma tulpa

--- pratica com python/Bibliotecas-Python/Exericios_ListaEmLista.py
+++ pratica com python/Bibliotecas-Python/Exericios_ListaEmLista.py
@@ -9,3 +9,14 @@ for linha in lista_de_listas:
         soma = soma + numero
         
     print(f'{soma}')
+
+#2. Crie um código para gerar uma lista que armazena o terceiro elemento de cada tupla contida na seguinte lista de tuplas:
+
+lista_de_tuplas = [('Pedro', 1.74, 81), ('Júlia', 1.65, 67), ('Otávio', 1.81, 83)]
+idades = []
+
+for idade in lista_de_tuplas:
+    idades.append(idade[2])
+
+print(f'{idades}')
+
