ef9904a4ef3f4fecf31d6a9583fcde6dbe8a5658
Viniciusxz <viniciusribeirolinsr@gmail.com>
2026-03-11 16:59:09 -0300

    Sistema que cria tulpa com base em condições

--- pratica com python/Bibliotecas-Python/Exericios_ListaEmLista.py
+++ pratica com python/Bibliotecas-Python/Exericios_ListaEmLista.py
@@ -49,3 +49,26 @@ vendas = [('2023', 4093), ('2021', 4320), ('2021', 5959), ('2022', 8883), ('2023
 
 vendas_ofc = [x for x in vendas if x[0] == '2022' and x[1] > 6000]
 print(vendas_ofc)
+
+#7.Uma clínica analisa dados de pacientes e armazena o valor numérico da glicose em um banco de dados e gostaria de rotular os dados da seguinte maneira:
+
+#Glicose igual ou inferior a 70: 'Hipoglicemia'
+#Glicose entre 70 a 99: 'Normal'
+#Glicose entre 100 e 125: 'Alterada'
+#Glicose superior a 125: 'Diabetes'
+#A clínica disponibilizou parte dos valores e sua tarefa é criar uma lista de tuplas usando list comprehension contendo o rótulo e o valor da glicemia em cada tupla.
+
+glicemia = [129, 82, 60, 97, 101, 65, 62, 167, 87, 53, 58, 92, 66, 120, 109, 62, 86, 96, 103, 88, 155, 52, 89, 73]
+
+sistema_glicemia = [
+    (
+    'Hipoglicemia' if valor <= 70
+    else 'Normal' if valor <= 99
+    else 'Alterada' if valor <= 125
+    else 'Diabetes',
+    valor
+    )
+  for valor in glicemia
+]
+
+print(sistema_glicemia)
