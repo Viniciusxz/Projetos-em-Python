894289be3072d0a9a2a3133897d3e9bf7507bb75
Viniciusxz <viniciusribeirolinsr@gmail.com>
2026-03-11 16:08:26 -0300

    Sistema de Lista de uma estrutura especifica, e uma criação de dicionario com 2 listas.

--- pratica com python/Bibliotecas-Python/Exericios_ListaEmLista.py
+++ pratica com python/Bibliotecas-Python/Exericios_ListaEmLista.py
@@ -20,3 +20,25 @@ for idade in lista_de_tuplas:
 
 print(f'{idades}')
 
+#3. A partir da lista: lista = ['Pedro', 'Júlia', 'Otávio', 'Eduardo'], crie um código para gerar uma lista de tuplas em que cada tupla tenha o primeiro elemento como a posição do nome na lista original e o segundo elemento sendo o próprio nome.
+
+lista = ['Pedro', 'Júlia', 'Otávio', 'Eduardo']
+codigo_lista = []
+
+for elemento, pessoa in enumerate(lista):
+    codigo_lista.append((elemento, pessoa))
+
+print(codigo_lista)
+
+#4. Crie uma lista usando o list comprehension que armazena somente o valor numérico de cada tupla caso o primeiro elemento seja 'Apartamento', a partir da seguinte lista de tuplas:
+aluguel = [('Apartamento', 1700), ('Apartamento', 1400), ('Casa', 2150), ('Apartamento', 1900), ('Casa', 1100)]
+
+apartamento_ofc = [n[1] for n in aluguel if n[0] == 'Apartamento']
+print(apartamento_ofc)
+
+#5. Crie um dicionário usando o dict comprehension em que as chaves estão na lista meses = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'] e os valores estão em despesa = [860, 490, 1010, 780, 900, 630, 590, 770, 620, 560, 840, 360].
+meses = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+despesa = [860, 490, 1010, 780, 900, 630, 590, 770, 620, 560, 840, 360]
+
+despesa_anual = {meses[i]: despesa[i] for i in range(len(meses))}
+print(despesa_anual)
