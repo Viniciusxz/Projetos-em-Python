7809dfb4fdf20183ea2f3d77d6e7b9015f381824
Viniciusxz <viniciusribeirolinsr@gmail.com>
2026-03-11 16:18:29 -0300

    Sistema que cria uma lista que filtra ano e valores

--- pratica com python/Bibliotecas-Python/Exericios_ListaEmLista.py
+++ pratica com python/Bibliotecas-Python/Exericios_ListaEmLista.py
@@ -42,3 +42,10 @@ despesa = [860, 490, 1010, 780, 900, 630, 590, 770, 620, 560, 840, 360]
 
 despesa_anual = {meses[i]: despesa[i] for i in range(len(meses))}
 print(despesa_anual)
+
+#6. Uma loja possui um banco de dados com a informação de venda de cada representante e de cada ano e precisa filtrar somente os dados do ano 2022 com venda maior do que 6000. A loja forneceu uma amostra contendo apenas as colunas com os anos e os valores de venda para que você ajude a realizar a filtragem dos dados a partir de um código:
+vendas = [('2023', 4093), ('2021', 4320), ('2021', 5959), ('2022', 8883), ('2023', 9859), ('2022', 5141), ('2022', 7688), ('2022', 9544), ('2023', 4794), ('2021', 7178), ('2022', 3030), ('2021', 7471), ('2022', 4226), ('2022', 8190), ('2021', 9680), ('2022', 5616)]
+# Crie uma lista usando list comprehension para filtrar os valores de 2022 e que sejam maiores que 6000.
+
+vendas_ofc = [x for x in vendas if x[0] == '2022' and x[1] > 6000]
+print(vendas_ofc)
